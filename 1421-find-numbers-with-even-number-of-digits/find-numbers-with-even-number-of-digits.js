be0301623f5f3db6343e2faf139ccf4c9a69645e
Muhammed Haneen <135997468+haneenpk@users.noreply.github.com>
2024-07-27 16:40:39 +0530

    Time: 55 ms (58.50%) | Memory: 50.5 MB (48.56%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
@@ -6,13 +6,7 @@ var findNumbers = function(nums) {
     let MCount = 0
     for(let i=0; i<nums.length; i++){
         let str = nums[i].toString()
-        // console.log(nums[i])
-        let count = 0
-        for(let j=1; j<=str.length; j++){
-            count++
-        }
-        console.log(count)
-        if(count !== 0 && count % 2 === 0){
+        if(str.length % 2 === 0){
             MCount++
         }
     }
